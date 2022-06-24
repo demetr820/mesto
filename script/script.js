e229b5b0ca7f28ee1e57563ab83b213e2142b10c
Dmitry Gordeev <dgordeev335@gmail.com>
2022-06-24 15:07:46 +0300

    fix script

--- script/script.js
+++ script/script.js
@@ -4,6 +4,9 @@ const buttonClose = popup.querySelector('.popup__button-close');
 const inputUserName = popup.querySelector('#user-name');
 const inputAbout = popup.querySelector('#about');
 const form = popup.querySelector('.popup__container');
+const popupHide = () => {
+  popup.classList.remove('popup_opened');
+}
 
 buttonEdit.addEventListener('click', function() {
   const userName = document.querySelector('.profile__user-name');
@@ -12,9 +15,7 @@ buttonEdit.addEventListener('click', function() {
   inputAbout.value = userDescription.textContent;
   popup.classList.add('popup_opened');
 });
-buttonClose.addEventListener('click', function() {
-  popup.classList.remove('popup_opened');
-});
+buttonClose.addEventListener('click', popupHide);
 form.addEventListener('submit', function(evt) {
   evt.preventDefault();
   const inputUserName = popup.querySelector('#user-name');
@@ -22,6 +23,6 @@ form.addEventListener('submit', function(evt) {
   const userName = document.querySelector('.profile__user-name');
   const userDescription = document.querySelector('.profile__description');
   userName.textContent = inputUserName.value;
-  console.log(userName.textContent);
   userDescription.textContent = inputAbout.value;
+  popupHide();
 });
